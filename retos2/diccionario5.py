9a06f02cfdd93b6ea0c84e2407f5e054dc9f5742
eniacbot <eniacbot@gmail.com>
2024-11-09 16:20:36 -0500

    rainbow para csv creado desde el dataframe

--- retos2/diccionario5.py
+++ retos2/diccionario5.py
@@ -1,9 +1,41 @@
+import pandas as pd
+
 # Crear un diccionario de una persona (Peter Parker)
 persona = {
     "nombre": "Peter Parker",
     "edad": 18,
     "ciudad": "Nueva York"
 }
+
+personas = [
+    {"nombre": "Peter Parker", "edad": 18, "ciudad": "Nueva York"},
+    {"nombre": "Tony Stark", "edad": 48, "ciudad": "Malibu"},
+    {"nombre": "Natasha Romanoff", "edad": 35, "ciudad": "San Petersburgo"},
+    {"nombre": "Bruce Banner", "edad": 45, "ciudad": "Dayton"},
+    {"nombre": "Steve Rogers", "edad": 102, "ciudad": "Brooklyn"},
+    {"nombre": "Diana Prince", "edad": 900, "ciudad": "Themyscira"},
+    {"nombre": "Clark Kent", "edad": 36, "ciudad": "Smallville"},
+    {"nombre": "Barry Allen", "edad": 30, "ciudad": "Central City"},
+    {"nombre": "Arthur Curry", "edad": 34, "ciudad": "Atlantis"},
+    {"nombre": "Victor Stone", "edad": 27, "ciudad": "Detroit"}
+]
+
+# Crear el DataFrame con pandas
+df_personas = pd.DataFrame(personas)
+df_personas
+
+# Crear el DataFrame
+df_personas = pd.DataFrame(personas)
+print(df_personas)
+
+print("primeras cinco filas de dataframe")
+print(df_personas.head())  # Muestra las primeras 5 filas
+
+
+print(df_personas.describe())
+
+df_personas.to_csv("personas.csv", index=False)
+
 
 # Acceder a valores
 print("Nombre:", persona["nombre"])  # Peter Parker
