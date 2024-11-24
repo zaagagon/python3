261b352c28c6b06a419e4d271aeb5b28152fdea5
zaagagon <zaagagon@gmail.com>
2024-11-24 16:42:21 -0500

    indice  nombre[1]

--- ejemplos_basicos/foriter.py
+++ ejemplos_basicos/foriter.py
@@ -1,3 +1,8 @@
 lista=["hola",2,"carro",5,34]
 for elem in lista:
     print(elem)
+    
+print
+#for con serie de numeros
+print("hola de prueba")
+  
--- ejemplos_basicos/forlist.py
+++ ejemplos_basicos/forlist.py
@@ -3,3 +3,4 @@ nombres = ["Alice", "Bob", "Charlie"]
 
 for nombre in nombres:
     print(nombre)
+print(nombre[1])
