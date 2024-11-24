5962409ea6f363be2f9b6631188dc2faa697df15
zaagagon <zaagagon@gmail.com>
2024-11-24 16:49:59 -0500

    sale h por charlie con nombre

--- ejemplos_basicos/forlist.py
+++ ejemplos_basicos/forlist.py
@@ -3,7 +3,8 @@ nombres = ["Alice", "Bob", "Charlie"]
 
 for nombre in nombres:
     print(nombre)
+#muestra el contenido de nombre es decir h porque es posicio 1
 print(nombre[1])
-
+#muestra indice valor de la lista
 for indice,elem in enumerate(nombres):
     print(indice,elem)
