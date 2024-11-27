3664de915dbd46eee83b6e2de01a1a3ef3d49925
zaagagon <zaagagon@gmail.com>
2024-11-26 22:32:58 -0500

    crearcito full

--- diccionarios/crearcito.py
+++ diccionarios/crearcito.py
@@ -5,17 +5,10 @@ import os
 directory = "/Volumes/kevin/1/python3/diccionarios"
 os.makedirs(directory, exist_ok=True)  # Crear la carpeta si no existe
 file_path = os.path.join(directory, "marvelianoski.json")
-
 # Crear un diccionario para personajes de Marvel
 marvelianos = {
-    "nickname": "mujer maravilla",
-    "nombre_real": "brenda",
-    "clase": "SuperHumana",
-    "habilidades": ["fuerza", "me ama"]
 }
-
 # Guardar el diccionario en el archivo JSON
 with open(file_path, "w") as file:
     json.dump(marvelianos, file, indent=4)
-
 print(f"El archivo JSON se ha creado en: {file_path}")
