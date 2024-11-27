596370a4ed9109c306bafbdaf0af3bd71b6442df
zaagagon <zaagagon@gmail.com>
2024-11-26 21:03:14 -0500

    crearcito okis function mujer maravilla

--- diccionarios/crearcito.py
+++ diccionarios/crearcito.py
@@ -4,13 +4,18 @@ import os
 # Ruta del directorio donde se guardará el archivo JSON
 directory = "/Volumes/kevin/1/python3/diccionarios"
 os.makedirs(directory, exist_ok=True)  # Crear la carpeta si no existe
-file_path = os.path.join(directory, "marvelianoss.json")
+file_path = os.path.join(directory, "marvelianoski.json")
 
-# Crear un diccionario vacío para personajes de Marvel
+# Crear un diccionario para personajes de Marvel
 marvelianos = {
-    "nickname":"mujer maravilla",
-    "nombre_real":"brenda",
-    "clase":"SuperHumana",
-    "habilidades":["fuerza","me ama"]
-    
+    "nickname": "mujer maravilla",
+    "nombre_real": "brenda",
+    "clase": "SuperHumana",
+    "habilidades": ["fuerza", "me ama"]
 }
+
+# Guardar el diccionario en el archivo JSON
+with open(file_path, "w") as file:
+    json.dump(marvelianos, file, indent=4)
+
+print(f"El archivo JSON se ha creado en: {file_path}")
