b65ad883e0092da0e78838d5dcae27b5c990fdb8
zaagagon <zaagagon@gmail.com>
2025-09-14 01:11:49 -0500

    arreglos python

---
+++ arrays/arrays1.py
@@ -0,0 +1,15 @@
+# Crear una lista
+numeros = [1, 2, 3, 4, 5]
+
+# Acceso
+print(numeros[0])   # Primer elemento
+print(numeros[-1])  # Último elemento
+
+# Operaciones
+print(len(numeros))       # Longitud
+print(sum(numeros))       # Suma
+print(max(numeros))       # Máximo
+print(min(numeros))       # Mínimo
+
+# Rebanado (slicing)
+print(numeros[1:4])  # [2, 3, 4]
