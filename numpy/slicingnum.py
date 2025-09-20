c487a52c902a6c6988a5d0306368e9a2a60fd817
zaagagon <zaagagon@gmail.com>
2025-09-19 23:57:15 -0500

    slicing

---
+++ numpy/slicingnum.py
@@ -0,0 +1,8 @@
+import numpy as np
+
+arr = np.arange(10)  # [0 1 2 3 4 5 6 7 8 9]
+
+print(arr[0])    # Primer elemento
+print(arr[-1])   # Último elemento
+print(arr[2:7])  # Del índice 2 al 6
+print(arr[::2])  # Saltando de 2 en 2
