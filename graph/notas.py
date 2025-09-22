5fa5629188ba463acef306376243c803c5699048
zaagagon <zaagagon@gmail.com>
2025-09-21 23:56:36 -0500

    notas1

---
+++ graph/notas.py
@@ -0,0 +1,11 @@
+import matplotlib.pyplot as plt
+#notas
+
+notas={
+    "Alicia":{"notas":{4.5,6,8,10}},
+    "Ramon":{"notas":{6.5,7,9,10}},
+    "Felipe":{"notas":{5.5,9,8.4,10}}
+}
+
+#graficar
+
