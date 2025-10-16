198d80d7b3d0d298b98057f0463411983e686a66
zaagagon <zaagagon@gmail.com>
2025-10-15 22:53:21 -0500

    lista heroes

---
+++ basic/hola.py
@@ -0,0 +1,10 @@
+heroes = [ "iron", "thor"]
+print(heroes[1])
+heroes.append("hulk")
+heroes.insert(0,"america")
+print("lista :", heroes)
+# Hola mundo en Python
+print("Hola, mundo!")
+# Fin del programa
+# Hola mundo en Python
+print("Hola, mundo!")
---
+++ basic/lista3.py
@@ -0,0 +1,3 @@
+verduras = []
+
+verduras.
